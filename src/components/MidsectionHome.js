bfe497653100231dd52032b5673e7b80a24d3e1c
cindykandie <cindyjk6@gmail.com>
2023-05-02 21:08:35 +0200

    Configure midsection on hope page

--- src/components/MidsectionHome.js
+++ src/components/MidsectionHome.js
@@ -4,6 +4,7 @@ import React from "react";
 export default function MidsectionHome() {
   return (
     <div>
+
       <section className="relative h-screen ">
         <img
           src="/assets/bg-image.png"
@@ -12,7 +13,7 @@ export default function MidsectionHome() {
         />
         <div className="absolute inset-0 flex items-center justify-left ml-16">
           <h2 className="text-white font-bold text-1xl md:text-2xl lg:text-3xl text-left px-6 w-20 mx-2">
-            WELCOME TO THE <span className="text-red-600">EXTENDERS</span>{" "}
+            WELCOME TO THE <span className="text-red-600">EXTENDERS </span>
             FOUNDATION
           </h2>
         </div>
@@ -34,7 +35,7 @@ export default function MidsectionHome() {
           quo debitis suscipit?
         </p>
       </section>
-
+      
       <section className="relative h-screen">
         <img
           src="/assets/bg-image2.png"
