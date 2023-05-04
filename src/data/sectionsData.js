2df0d7c0dae28807ad00666b4ece5ceadf05fb65
cindykandie <cindyjk6@gmail.com>
2023-05-04 13:29:24 +0300

    Complete the get involved page

---
+++ src/data/sectionsData.js
@@ -0,0 +1,39 @@
+export const sectionsData = [
+    {
+      id: 1,
+      title: "WHO WE ARE",
+      subtitle: "OUR IMPACT",
+      description:
+        "Lorem Ipsum Lorem Ipsum Loren IPsum Loren Ipsum Lorem Ipsum Lorem Ipsum Loren IPsum Loren Ipsum Lorem Ipsum Lorem Ipsum Loren IPsum Loren Ipsum Lorem Ipsum Loren IPsum Loren Ipsum",
+      image: "/img3.png",
+      imageAlt: "Image 1",
+    },
+    {
+      id: 2,
+      title: "WHAT WE DO",
+      subtitle: "OUR SERVICES",
+      description:
+        "Lorem Ipsum Lorem Ipsum Loren IPsum Loren Ipsum Lorem Ipsum Lorem Ipsum Loren IPsum Loren Ipsum Lorem Ipsum Lorem Ipsum Loren IPsum Loren Ipsum Lorem Ipsum Loren IPsum Loren Ipsum",
+      image: "/img3.png",
+      imageAlt: "Image 2",
+    },
+    {
+      id: 3,
+      title: "OUR MISSION",
+      subtitle: "OUR VISION",
+      description:
+        "Lorem Ipsum Lorem Ipsum Loren IPsum Loren Ipsum Lorem Ipsum Lorem Ipsum Loren IPsum Loren Ipsum Lorem Ipsum Lorem Ipsum Loren IPsum Loren Ipsum Lorem Ipsum Loren IPsum Loren Ipsum",
+      image: "/img3.png",
+      imageAlt: "Image 3",
+    },
+    {
+      id: 4,
+      title: "GET INVOLVED",
+      subtitle: "EXTENDERS FOUNDATION",
+      description:
+        "Lorem Ipsum Lorem Ipsum Loren IPsum Loren Ipsum Lorem Ipsum Lorem Ipsum Loren IPsum Loren Ipsum Lorem Ipsum Lorem Ipsum Loren IPsum Loren Ipsum Lorem Ipsum Loren IPsum Loren Ipsum",
+      image: "/img3.png",
+      imageAlt: "Image 4",
+    },
+  ];
+  
