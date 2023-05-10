83edbcaa14d5b7b625283d63940d9f2db291e86b
mbuimbogo <mbuimbogo99@gmail.com>
2023-05-10 12:25:18 +0300

    Update core values texts

--- src/data/members.jsx
+++ src/data/members.jsx
@@ -3,27 +3,27 @@ export const values = [
     {
         icon:"/icon6.jpeg",
         name:"EQUITY",
-        text:"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+        text:"We strive to create a community where everyone has an equal opportunity to thrive by providing targeted support and resources to those who are most in need",
     },
     {
         icon:"/icon4.jpeg",
         name:"ACCOUNTABILITY",
-        text:"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+        text:"We hold ourselves accountable to the highest standards of ethical conduct. We are dedicated to ensuring that every donation we receive is used in a manner consistent with our mission and beliefs.",
     },
     {
         icon:"icon1.jpeg",
         name:"COMMUNITY",
-        text:"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+        text:"Community is at the heart of everything we do. Working together is the most effective method to bring about positive change, thus we are dedicated to establishing solid, welcoming communities in which everyone may flourish.",
     },
     {
         icon:"icon3.jpeg",
         name:"TEAMWORK",
-        text:"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+        text:"We believe that by working collaboratively and supporting one another, we can overcome challenges more than we ever could alone.",
     },
     {
         icon:"icon2.jpeg",
         name:"KINDNESS",
-        text:"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+        text:"We believe in treating others with compassion, empathy, and the importance of extending kindness beyond our immediate circle and spreading love and positivity in our wider communities.",
     }
   ]
   
