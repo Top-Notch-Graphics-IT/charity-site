481fafd8633c2d3acaba9001ca1d53d975d29d3c
mbuimbogo <mbuimbogo99@gmail.com>
2023-05-12 10:45:45 +0300

    UPdate icon importations

--- src/data/members.jsx
+++ src/data/members.jsx
@@ -1,27 +1,27 @@
 
 export const values = [
     {
-        icon:"/assets/icon6.jpeg",
+        icon:"/icons/icon6.jpeg",
         name:"EQUITY",
         text:"We strive to create a community where everyone has an equal opportunity to thrive by providing targeted support and resources to those who are most in need",
     },
     {
-        icon:"/assets/icon4.jpeg",
+        icon:"/icons/icon4.jpeg",
         name:"ACCOUNTABILITY",
         text:"We hold ourselves accountable to the highest standards of ethical conduct. We are dedicated to ensuring that every donation we receive is used in a manner consistent with our mission and beliefs.",
     },
     {
-        icon:"/assets/icon1.jpeg",
+        icon:"/icons/icon1.jpeg",
         name:"COMMUNITY",
         text:"Community is at the heart of everything we do. Working together is the most effective method to bring about positive change, thus we are dedicated to establishing solid, welcoming communities in which everyone may flourish.",
     },
     {
-        icon:"/assets/icon3.jpeg",
+        icon:"/icons/icon3.jpeg",
         name:"TEAMWORK",
         text:"We believe that by working collaboratively and supporting one another, we can overcome challenges more than we ever could alone.",
     },
     {
-        icon:"/assets/icon2.jpeg",
+        icon:"/icons/icon2.jpeg",
         name:"KINDNESS",
         text:"We believe in treating others with compassion, empathy, and the importance of extending kindness beyond our immediate circle and spreading love and positivity in our wider communities.",
     }
